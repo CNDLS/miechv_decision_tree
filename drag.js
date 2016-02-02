a5b24e50625651cdcbc901742f15f74eb7593174
Julie Salah <jms552@georgetown.edu>
2016-02-02 17:04:08 -0500

    adding Bills changes from working session 2/2

--- drag.js
+++ drag.js
@@ -16,10 +16,10 @@ var c= {
   // we'll be dragging a copy of the clicked item.
   thumb: null,
   option_title: null,
-	accept: null,
-	overlap: 0.1,
-	
-
+  accept: null,
+  
+  // we keep track of whether it was dropped somewhere good.
+  dropped_onto_target: false,
   
   // create a copy of the clicked item.
   onstart: function (evt) {
@@ -40,8 +40,6 @@ var c= {
     c.thumb.style.top = orig_rect.top + "px";
     c.thumb.style.width = (orig_rect.right - orig_rect.left) + "px";
   },
-
-
 
   onmove: function (evt) {
     // keep the dragged position in the data-x/data-y attributes
@@ -64,70 +62,47 @@ var c= {
     if (c.drop_target.title == c.option_title) {
       c.drop_target.classList.add("hovered");
       c.drop_target.parentNode.classList.add("hovered");
-	  c.dragged_element.classList.add('drop-target');
-	  c.dragged_element.classList.add('can-drop');
-	  c.dragged_element.textContent = 'Dragged in';
+	  c.dropped_onto_target = true;
+    }
+  },
+
+  ondragleave: function (evt) {
+    var activities = document.getElementsByClassName("activity");
+    var activity;
+    for (var i=0; i <= activities.length; i++) {
+      activity = activities[i];
+      if (activity === undefined) continue;
+      activity.classList.remove("hovered");
+      for (var j=0; j <= activity.children.length; j++) {
+        if (activity.children[j] === undefined) continue;
+        activity.children[j].classList.remove("hovered");
+      }
     }
   },
   
   ondrop: function (evt) 
   {
-     c.dragged_element = evt.relatedTarget;
-     c.drop_target = evt.target;
-     if (c.drop_target.title == c.option_title) 
-	 {
-       c.thumb = document.body.removeChild(c.thumb);
-       c.thumb.style.left = c.thumb.style.top = "0";
-       c.drop_target.appendChild(c.dragged_element);
-	   c.drop_target.classList.remove('drop-target');
-	   c.dragged_element.textContent = 'Dropped';
-     }
-	 
-   },
- 
-
-
-  ondragleave: function (evt) 
-   {
-    var activities = document.getElementsByClassName("activity");
-    var activity;
-    for (var i=0; i <= activities.length; i++) 
-	{
-      activity = activities[i];
-      if (activity === undefined) continue;
-      activity.classList.remove("hovered");
-      for (var j=0; j <= activity.children.length; j++) 
-	  {
-        if (activity.children[j] === undefined) continue;
-        activity.children[j].classList.remove("hovered");
-		evt.target.classList.remove('drop-target');
-		evt.relatedTarget.classList.remove('can-drop');
-		evt.relatedTarget.classList.textContent = 'Dragged out';
-      }
-    }
-  },
-  
-//********************************************
-  
-  ondropactivate: function (evt) {
-      c.drop_target = evt.target;
-      if (c.drop_target.title == c.option_title) 
- 	 {
- 	   c.drop_target.classList.add('drop-active');
-      }
-
-   },
-		  
-ondropdeactivate: function (evt) {
+    c.dragged_element = evt.relatedTarget;
     c.drop_target = evt.target;
     if (c.drop_target.title == c.option_title) 
- 	{
-      c.drop_target.classList.remove('drop-target');
+	{
+      c.thumb = document.body.removeChild(c.thumb);
+	  var dropped_span = document.createElement("span");
+	  dropped_span.innerHTML = c.thumb.innerHTML;
+	  c.drop_target.appendChild(dropped_span);
     }
   },
-	 
+  
+  onend: function (evt) {
+     // if not dropped over a valid target, remove the draggable.
+     if (!c.dropped_onto_target) {
+       document.body.removeChild(c.thumb);
+     }
+     c.dropped_onto_target = false;
+   },
+  
 }
+
 
 interact('.well > span').draggable(c);
 interact('.droptarget').dropzone(c);
-
